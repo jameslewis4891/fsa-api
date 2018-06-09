1183f825a63ad5022ebdb8a9660736171d27e826
jlewis <james.lewis4891@gmail.com>
2018-06-09 21:51:22 +0100

    Added CORS configuration

---
+++ src/main/java/com/iw/fsaapi/configuration/CORSConfiguration.java
@@ -0,0 +1,24 @@
+package com.iw.fsaapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+
+    private static final String[] EXPOSED_HEADERS = {"origin","content-type","accept"};
+
+    private static final long maxAge = 36000;
+
+    @Override
+    public void addCorsMappings(final CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedMethods("GET","OPTIONS","HEAD")
+                .exposedHeaders(EXPOSED_HEADERS)
+                .allowedHeaders(EXPOSED_HEADERS)
+                .maxAge(maxAge)
+                .allowedOrigins("http://localhost:4200");
+    }
+
+}
