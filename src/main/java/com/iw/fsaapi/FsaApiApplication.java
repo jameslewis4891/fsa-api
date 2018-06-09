aa614a4779dcb8c11da0827f7064710ef7cecd0d
jlewis <james.lewis4891@gmail.com>
2018-06-09 09:39:05 +0100

    Moved files from spring initializer

---
+++ src/main/java/com/iw/fsaapi/FsaApiApplication.java
@@ -0,0 +1,12 @@
+package com.iw.fsaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsaApiApplication.class, args);
+	}
+}
